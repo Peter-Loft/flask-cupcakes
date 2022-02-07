5bf673c62ffab35d86a57f0db68f8b8e78632a36
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-02-07 14:18:28 -0600

    Create first model and troubleshooted the seed.py

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy, Model
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """ Cupcake Model """
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String, nullable=False)
+    size = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String, default=DEFAULT_IMAGE, nullable=False)
